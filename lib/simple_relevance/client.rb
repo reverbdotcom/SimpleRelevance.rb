8ce1089766773547dce8531aba5bbe3c2248f364
Yan Pritzker <yan@pritzker.ws>
2014-05-12 14:56:33 -0500

    Added support for bulk actions

--- lib/simple_relevance/client.rb
+++ lib/simple_relevance/client.rb
@@ -12,7 +12,7 @@ module SimpleRelevance
   class Client
     include HTTParty
 
-    def initialize(username=ENV["SIMPLE_RELEVANCE_USERNAME"] , api_key=ENV["SIMPLE_RELEVANCE_API_KEY"] , async=1)
+    def initialize(username=ENV["SIMPLE_RELEVANCE_USERNAME"] , api_key=ENV["SIMPLE_RELEVANCE_API_KEY"], async=1)
       @async = async
       @basic_auth = {password: api_key, username: username}
     end
@@ -21,20 +21,27 @@ module SimpleRelevance
     # Useful for use with e.g. sidekiq delayed extensions
     #
     # e.g. SimpleRelevance.delay.call_api(username: "foo", api_key: "bar", method: :get_user, opts: { email: "foo@bar.com" }
-    def self.call_api(username: ENV["SIMPLE_RELEVANCE_USERNAME"], api_key: ENV["SIMPLE_RELEVANCE_API_KEY"], method:, opts:{})
-      new(username, api_key).send(method, opts)
+    def self.call_api(username: ENV["SIMPLE_RELEVANCE_USERNAME"], api_key: ENV["SIMPLE_RELEVANCE_API_KEY"], method:, async: 1, opts:{})
+      new(username, api_key, async).send(method, opts)
     end
 
+    # Required keys:
+    #  email, user_id
+    #
     # Reserved keys:
     # -first_name (users only)
     # -last_name (users only)
     # -twitter_handle (users only)
     # -image_url (users and items)
+    #
     def add_user(opts={})
+      self._post('users/', user_payload(opts))
+    end
+
+    # Takes an array of user hashes. The hashes must look like users you would pass to #add_user
+    def batch_add_user(users=[])
       payload = {
-        email:     opts.delete(:email)   || raise("email is required"),
-        user_id:   opts.delete(:user_id) || raise("user_id is required"),
-        data_dict: opts
+        batch: users.map { |user_info| user_payload(user_info) }
       }
 
       self._post('users/', payload)
@@ -44,6 +51,9 @@ module SimpleRelevance
       self._get('users', opts)
     end
 
+    # Required Keys:
+    # item_name, item_ido
+    #
     # Reserved Key List:
     # -latitude
     # -longitude
@@ -64,13 +74,15 @@ module SimpleRelevance
     # -external_id (variants only)
     # -discount (items and variants only) - this can be in a variety of formats. "2%",20,.2,".2" - any string or number larger than 1 will be treated as "$$ off" and used along with price info to calculate percent discount.
     def add_item(opts={})
+      self._post('items/', item_payload(opts))
+    end
+
+    # Takes an array of hashes. The hashes must look like items you would pass to #add_item
+    def batch_add_item(items=[])
       payload = {
-        item_id:    opts.delete(:item_id)   || raise("item_id is required"),
-        item_name:  opts.delete(:item_name) || raise("item_name is required"),
-        item_type:  opts.delete(:item_type) || 'product',
-        variants:   opts.delete(:variants),
-        data_dict:  opts
+        batch: items.map { |item_info| item_payload(item_info) }
       }
+
       self._post('items/', payload)
     end
 
@@ -78,12 +90,27 @@ module SimpleRelevance
       add_action(opts.merge(action_type: ActionType::CLICK))
     end
 
+    def batch_add_clicks(clicks=[])
+      clicks.map! {|click_info| click_info.merge!(action_type: ActionType::CLICK)}
+      batch_add_action(clicks)
+    end
+
     def add_purchase(opts={})
       add_action(opts.merge(action_type: ActionType::PURCHASE))
     end
 
+    def batch_add_purchases(purchases=[])
+      purchases.map! {|purchase_info| purchase_info.merge!(action_type: ActionType::PURCHASE)}
+      batch_add_action(purchases)
+    end
+
     def add_email_open(opts={})
       add_action(opts.merge(action_type: ActionType::EMAIL_OPEN))
+    end
+
+    def batch_add_email_opens(email_opens=[])
+      email_opens.map! {|email_open_info| email_open_info.merge!(action_type: ActionType::EMAIL_OPEN)}
+      batch_add_action(email_opens)
     end
 
     def get_predictions(opts={})
@@ -108,11 +135,45 @@ module SimpleRelevance
     # Highly suggested parameters include timestamp (in UTC), price, zipcode, and,
     # if you are matching a preexisting item by name and not by item_id, item_type.
     def add_action(opts={})
+      self._post("actions/", action_payload(opts))
+    end
+
+    def batch_add_action(actions=[])
+      payload = {
+        batch: actions.map { |action_info| action_payload(action_info) }
+      }
+
+      puts payload.inspect
+
+      self._post('actions/', payload)
+    end
+
+    def user_payload(opts)
+      {
+        email:     opts.delete(:email)   || raise("email is required"),
+        user_id:   opts.delete(:user_id) || raise("user_id is required"),
+        data_dict: opts
+      }
+    end
+
+    def item_payload(opts)
+      opts[:item_url]  || raise("item_url is required and must point to an internet accessible url")
+      opts[:image_url] || raise("image_url is required")
+
+      {
+        item_id:    opts.delete(:item_id)   || raise("item_id is required"),
+        item_name:  opts.delete(:item_name) || raise("item_name is required"),
+        variants:   opts.delete(:variants),
+        item_type:  opts[:item_type] || 'product',
+        data_dict:  opts
+      }
+    end
+
+    def action_payload(opts)
       opts[:item_id] || raise("item_id is required")
       opts[:user_id] || raise("user_id is required")
       opts[:action_type] || raise("action_type is required")
-
-      self._post("actions/", opts)
+      opts
     end
   end
 end
---
+++ spec/lib/simple_relevance/client_spec.rb
@@ -0,0 +1,121 @@
+# Use this to test your connection to simple relevance
+# set ENV variables for SIMPLE_RELEVANCE_USERNAME and SIMPLE_RELEVANCE_API_KEY
+#
+# Example:
+# (in ~/.profile)
+#
+#   export SIMPLE_RELEVANCE_USERNAME=reverb0
+#   export SIMPLE_RELEVANCE_API_KEY=bff9ba28c7cf4d645503
+#
+$LOAD_PATH << "."
+require 'lib/simple_relevance'
+require 'spec_helper'
+require 'rspec/expectations'
+
+RSpec::Matchers.define :be_successful do |expected|
+  match do |actual|
+    puts actual["code"]
+    actual["message"] == "success"
+  end
+end
+
+describe SimpleRelevance::Client do
+  use_vcr_cassette
+
+  let(:email) { "user1@foo.com" }
+  let(:email2) { "user2@foo.com" }
+  let(:username) { ENV["SIMPLE_RELEVANCE_USERNAME"] }
+  let(:api_key) { ENV["SIMPLE_RELEVANCE_API_KEY"] }
+
+  let(:sr) do
+    if !username || !api_key
+      raise "Please specify SIMPLE_RELEVANCE_API_KEY and SIMPLE_RELEVANCE_USERNAME in your environment to run tests."
+    end
+
+    described_class.new(username, api_key, async=0)
+  end
+
+  it "adds users" do
+    sr.add_user(email: email, user_id: 1, some_property: "my value").should be_successful
+  end
+
+  it "batch adds users" do
+    sr.batch_add_user(
+      [
+        { email: email, user_id: 1, some_property: "my value" },
+        { email: email2, user_id: 2, some_property: "some prop" }
+      ]
+    ).should be_successful
+  end
+
+  it "retreives users" do
+    sr.get_user(user_email: email)["total"].should == 1
+  end
+
+  it "adds items" do
+    sr.add_item(
+      item_name: "someitem",
+      item_id: "100-foo",
+      testattr: "wahoo",
+      item_url: "http://foo.com/bar",
+      image_url: "http://google.com/some.png"
+    ).should be_successful
+  end
+
+  it "batch adds items" do
+    sr.batch_add_item([
+      {
+        item_name: "foo-item1",
+        item_id: "101-foo",
+        testattr: "wahoo",
+        item_url: "http://foo.com/bar1",
+        image_url: "http://google.com/some.png"
+      },
+      {
+        item_name: "foo-item2",
+        item_id: "102-foo",
+        testattr: "wahoo",
+        item_url: "http://foo.com/bar2",
+        image_url: "http://google.com/some.png"
+      }
+    ]).should be_successful
+  end
+
+  it "tracks clicks" do
+    # sr.add_click(user_id: 1, item_id: 1).should be_successful
+    sr.add_click(user_id: 1, item_id: "101-foo").should be_successful
+    sr.add_click(user_id: 1, item_id: "102-foo").should be_successful
+  end
+
+  it "batch tracks clicks" do
+    sr.batch_add_clicks([
+      { user_id: 100, item_id: "101-foo"},
+      { user_id: 101, item_id: "102-foo"}
+    ]).should be_successful
+  end
+
+  it "tracks email opens" do
+    sr.add_email_open(user_id: 1, item_id: 1).should be_successful
+  end
+
+  it "batch tracks email opens" do
+    sr.batch_add_email_opens([
+      { user_id: 100, item_id: "101-foo"},
+      { user_id: 101, item_id: "102-foo"}
+    ]).should be_successful
+  end
+
+  it "tracks purchases" do
+    sr.add_purchase(user_id: 1, item_id: 1).should be_successful
+  end
+
+  describe "class level api call" do
+    it "adds users" do
+      described_class.call_api(username: username, api_key: api_key, method: :add_user, opts: {
+        user_id: 1,
+        email: "foo@bar.com"
+      }).should be_successful
+    end
+
+  end
+end
